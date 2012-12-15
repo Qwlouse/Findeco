235ccf3e8c1c425df266831cf050641f50c8518a
Qwlouse <klaus.greff@gmx.net>
2012-12-15 23:50:33 +0100

    added licenses

--- codebar/settings.py
+++ codebar/settings.py
@@ -1,5 +1,30 @@
-#!/usr/bin/env python
-# encoding: utf-8
+#!/usr/bin/python
+# coding=utf-8
+# CoDebAr is dually licensed under GPLv3 or later and MPLv2.
+#
+################################################################################
+# Copyright (c) 2012 Klaus Greff <klaus.greff@gmx.net>
+# This file is part of CoDebAr.
+#
+# CoDebAr is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# CoDebAr is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# CoDebAr. If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+#
+################################################################################
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+################################################################################
+
 # Django settings for codebar project.
 from __future__ import division, print_function, unicode_literals
 
