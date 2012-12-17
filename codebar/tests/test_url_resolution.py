653b400f5ac73844adf557590b90f3bc9fe03cc9
pinae <pinae@web.de>
2012-12-17 21:15:45 +0100

    test for markNode

--- codebar/tests/test_url_resolution.py
+++ codebar/tests/test_url_resolution.py
@@ -30,7 +30,7 @@ from django.core.urlresolvers import resolve, Resolver404
 import unittest
 
 from ..views import load_user_settings, load_index, load_graph_data, load_microblogging, load_text, load_user_info
-from ..views import login, logout
+from ..views import login, logout, mark_node
 
 ########################### Test the API calls #################################
 valid_routes = [
@@ -126,6 +126,27 @@ valid_routes = [
         func=logout,
         url_name='logout',
         kwargs=dict()),
+
+    #### markNode
+    dict(url='/.json_markNode/spam/some.1/path.2',
+        func=mark_node,
+        url_name='mark_node',
+        kwargs=dict(path='/some.1/path.2', mark_type='spam')),
+
+    dict(url='/.json_markNode/notspam/some.1/path.2',
+        func=mark_node,
+        url_name='mark_node',
+        kwargs=dict(path='/some.1/path.2', mark_type='notspam')),
+
+    dict(url='/.json_markNode/follow/some.1/path.2',
+        func=mark_node,
+        url_name='mark_node',
+        kwargs=dict(path='/some.1/path.2', mark_type='follow')),
+
+    dict(url='/.json_markNode/unfollow/some.1/path.2',
+        func=mark_node,
+        url_name='mark_node',
+        kwargs=dict(path='/some.1/path.2', mark_type='unfollow')),
     ]
 
 class UrlResolutionTest(unittest.TestCase):
