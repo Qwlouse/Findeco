96b27a688d9b2c8a4e4d6f517785e31a0a2f8017
pinae <pinae@web.de>
2012-12-18 21:09:50 +0100

    Microblogging App started

---
+++ microblogging/models.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# coding=utf-8
+# CoDebAr is dually licensed under GPLv3 or later and MPLv2.
+#
+################################################################################
+# Copyright (c) 2012 Johannes Merkert <jonny@pinae.net>
+# This file is part of CoDebAr.
+#
+# CoDebAr is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# CoDebAr is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# CoDebAr. If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+#
+################################################################################
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+################################################################################
+
+
+from django.db import models
+
+# Create your models here.
