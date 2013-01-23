2c8c46141136b1b366ff0ed9a2848d9c25f6f93e
pinae <pinae@web.de>
2013-01-23 22:02:15 +0100

    all tests running

--- findeco/tests/__init__.py
+++ findeco/tests/__init__.py
@@ -26,25 +26,25 @@
 ################################################################################
 from __future__ import division, print_function, unicode_literals
 
-#from test_profile import UserProfileTest
-#from test_path import PathRegExTest
-#from test_url_resolution import UrlResolutionTest
-#from test_views import ViewTest
+from test_profile import UserProfileTest
+from test_path import PathRegExTest
+from test_url_resolution import UrlResolutionTest
+from test_views import ViewTest
 from test_view_helpers import  CreateIndexNodeForSlotTest, CreateIndexNodeForArgumentTest
 from test_view_helpers import CreateUsersInfoTest, CreateUserSettingsTest, CreateGraphDataNodeForStructureNodeTest
-#from test_load_index import LoadIndexTest, LoadArgumentIndexTest
-#from test_load_text import LoadTextTest
-#from test_user_api import LoadUserInfoTest, LoadUserSettingsTest
+from test_load_index import LoadIndexTest, LoadArgumentIndexTest
+from test_load_text import LoadTextTest
+from test_user_api import LoadUserInfoTest, LoadUserSettingsTest
 
-#try:
-#    from microblogging.tests import *
-#except ImportError:
-#    pass
+try:
+    from microblogging.tests import *
+except ImportError:
+    pass
 
-#try:
-#    from node_storage.tests import *
-#except ImportError:
-#    pass
+try:
+    from node_storage.tests import *
+except ImportError:
+    pass
 
 
 
