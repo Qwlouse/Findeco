8d611dd9b39f49ee090ae6673365dd37bf3ad33c
Qwlouse <klaus.greff@gmx.net>
2013-01-27 23:29:07 +0100

    tidied up imports (again)

--- node_storage/tests/test_structure_parser.py
+++ node_storage/tests/test_structure_parser.py
@@ -24,17 +24,18 @@
 from __future__ import division, print_function, unicode_literals
 from django.test import TestCase
 from node_storage.structure_parser import validate_structure_schema
-import re
 
-from ..factory import create_user, create_slot, create_structureNode
-from ..factory import create_textNode, create_argument
+from ..factory import create_argument, create_slot, create_structureNode
+from ..factory import create_textNode, create_user
 from ..path_helpers import get_root_node
 from ..models import Node
-from ..structure_parser import strip_accents, substitute_umlauts
-from ..structure_parser import parse, InvalidWikiStructure
-from ..structure_parser import remove_unallowed_chars, getHeadingMatcher
-from ..structure_parser import turn_into_valid_short_title
 from ..structure_parser import create_structure_from_structure_node_schema
+from ..structure_parser import InvalidWikiStructure
+from ..structure_parser import parse
+from ..structure_parser import remove_unallowed_chars
+from ..structure_parser import strip_accents
+from ..structure_parser import substitute_umlauts
+from ..structure_parser import turn_into_valid_short_title
 
 class StructureParserTest(TestCase):
     def test_strip_accents(self):
