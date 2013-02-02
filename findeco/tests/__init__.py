97a3da447fd6443858855dcd8970e5c050bb7199
pinae <pinae@web.de>
2013-02-02 16:19:26 +0100

    load_graph_data test was missing from init

--- findeco/tests/__init__.py
+++ findeco/tests/__init__.py
@@ -37,6 +37,7 @@ from test_load_index import LoadIndexTest, LoadArgumentIndexTest
 from test_load_text import LoadTextTest
 from test_user_api import LoadUserInfoTest, LoadUserSettingsTest
 from test_store_text import StoreTextTest
+from test_load_graph_data import LoadGraphDataTest
 
 try:
     from microblogging.tests import *
