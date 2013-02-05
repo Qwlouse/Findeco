44fea5a14ada0b3f5cc787c7a5456675698291a5
pinae <pinae@web.de>
2013-02-05 22:35:19 +0100

    stuff

---
+++ static/js/structure_parser.js
@@ -0,0 +1,38 @@
+/* Findeco is dually licensed under GPLv3 or later and MPLv2.
+#
+################################################################################
+# Copyright (c) 2012 Johannes Merkert <jonny@pinae.net>,
+# Klaus Greff <klaus.greff@gmx.net>
+# This file is part of Findeco.
+#
+# Findeco is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Findeco is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Findeco. If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+#
+################################################################################
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+##############################################################################*/
+
+h1Start = "^\s*=(?P<title>[^=]+)=*\s*$"
+
+function parse(s, shortTitle) {
+    m = s.match(h1Start)
+    if(m.length > 0){
+        /*title = m.groups("title")[0]
+        title = title.partition("§")[0] /* silently remove attempt to set short_title in H1 */
+        s = s.replace(h1Start,"")
+    } else {
+        /*raise InvalidWikiStructure('Must start with H1 heading to set title')*/
+    }
+}
