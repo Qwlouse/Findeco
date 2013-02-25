a2980c6bde293afff3cc36a3cf3bbeafac95afe8
Qwlouse <klaus.greff@gmx.net>
2013-02-25 23:29:38 +0100

    added basic local_settings.py

---
+++ local_settings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# coding=utf-8
+# Findeco is dually licensed under GPLv3 or later and MPLv2.
+#
+################################################################################
+# Copyright (c) 2012 Klaus Greff <klaus.greff@gmx.net>
+# This file is part of Findeco.
+#
+# Findeco is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Findeco is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Findeco. If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+#
+################################################################################
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+################################################################################
+
+# Here you can overwrite some of the settings from settings.py.
+# Note: This file will be ignored by git.
+
+ADMIN_PASS = "1234"
