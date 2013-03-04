f21aca1798e49fc25ece46ba892accc029adfcc2
Justelex <mail@maik-nauheim.de>
2013-03-05 00:06:38 +0100

    added missing files for cookiehandling Fixes#35

--- static/js/class.login.js
+++ static/js/class.login.js
@@ -29,7 +29,6 @@ function ClassLogin() {}
 
 
 ClassLogin.prototype.form = {}; 
-ClassLogin.prototype.rqhandler= RqHandler;
 
 ClassLogin.prototype.checkKey = function (e) {
     if ( e.which == 13 ) {
@@ -84,25 +83,26 @@ ClassLogin.prototype.createTableFormField = function(name,input,targetTable,func
     
 }
 
-ClassLogin.prototype.setLoginButtonState = function() { 
-	 Login.root = $('#login')
+ClassLogin.prototype.setLoginButtonState = function() {  
+	Login.root = $('#login')
         .empty();
     if ( User.isLoggedIn() == true ) {
+    	//Show Logout Link
         $('<div class="button">Ausloggen</div>')
             .attr('style','margin-bottom: 10px;')
             .click(Login.logout)
             .appendTo(Login.root);
         
     }else{
-    // Show Login link.
-    $('<div class="button">Einloggen</div>')
-        .attr('style','margin-bottom: 10px;')
-        .click(Login.showLoginForm)
-        .appendTo(Login.root);
-    $('<div class="button">Registrieren</div>')
-        .attr('style','margin-bottom: 10px;')
-        .click(Login.showRegisterForm)
-        .appendTo(Login.root);
+    	// Show Login link.
+    	$('<div class="button">Einloggen</div>')
+        	.attr('style','margin-bottom: 10px;')
+        	.click(Login.showLoginForm)
+        	.appendTo(Login.root);
+    	$('<div class="button">Registrieren</div>')
+        	.attr('style','margin-bottom: 10px;')
+        	.click(Login.showRegisterForm)
+        	.appendTo(Login.root);
     }
 }
 
@@ -260,7 +260,7 @@ ClassLogin.prototype.submitLogin = function() {
         alert('Bitte fülle alle Felder aus!');
         return false;
     }
-    Login.rqhandler.post({
+    RqHandler.post({
         url: '.json_login',
         data: tmp,
         success: Login.handleRequest
@@ -277,7 +277,7 @@ ClassLogin.prototype.submitRecoveryByMail = function() {
         alert('Bitte fülle alle Felder aus!');
         return false;
     }
-    Login.rqhandler.post({
+    RqHandler.post({
         url: '.json_accountResetRequestByMail',
         data: tmp,
     });
@@ -293,7 +293,7 @@ ClassLogin.prototype.submitRecoveryByName = function() {
         alert('Bitte fülle alle Felder aus!');
         return false;
     }
-    Login.rqhandler.post({
+    RqHandler.post({
         url: '.json_accountResetRequestByName',
         data: tmp
     });
@@ -318,7 +318,7 @@ ClassLogin.prototype.submitRegister = function() {
         alert('Die Passwörter stimmen nicht überein');
         return false;
     }
-    Login.rqhandler.post({
+       RqHandler.post({
         url: '.json_accountRegistration',
         data: tmp
     });
@@ -329,7 +329,7 @@ ClassLogin.prototype.submitActivation = function(activationKey) {
 	var tmp = {
     	'activationKey': activationKey,
     };
-   	Login.rqhandler.post({
+	RqHandler.post({
         url: '.json_accountActivation',
         data: tmp
     	});
@@ -340,7 +340,7 @@ ClassLogin.prototype.submitRecovery = function(activationKey) {
  	var tmp = {
         'activationKey': activationKey,
     };
-   	Login.rqhandler.post({
+ 	RqHandler.post({
         url: '.json_accountResetConfirmation',
         data: tmp
     });
