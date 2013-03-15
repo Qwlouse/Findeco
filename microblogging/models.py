56fcc973f62778ac9093b26ecd9d383eefe3c639
Qwlouse <klaus.greff@gmx.net>
2013-03-15 23:54:30 +0100

    changed link path for users. Fixes #60

--- microblogging/models.py
+++ microblogging/models.py
@@ -80,7 +80,7 @@ def create_post(text, author, path = None):
         username = split_text[i]
         try:
             u = User.objects.get(username=username)
-            split_text[i] = '<a href="/.users/{0}">@{0}</a>'.format(username)
+            split_text[i] = '<a href="/#/{0}">@{0}</a>'.format(username)
             mentions.append(u)
         except User.DoesNotExist:
             split_text[i] = '@' + username
