4211b35b9819ad64b0e40482dbf70d3b016d6049
Qwlouse <klaus.greff@gmx.net>
2013-03-19 13:50:35 +0100

    some more microblogging tweaks

--- static/js/findeco.microblogCtrl.js
+++ static/js/findeco.microblogCtrl.js
@@ -7,6 +7,7 @@ function FindecoMicroblogCtrl($scope, FindecoService) {
     $scope.submit = function () {
         FindecoService.post({action: '.json_storeMicroblogPost/', 'microblogText': $scope.testinput}, function() {
             $scope.microbloggingList = FindecoService.get({action: '.json_loadMicroblogging', arg2: '0', arg3: 'newer'});
+            $scope.testinput = "";
         });
     };
 }
