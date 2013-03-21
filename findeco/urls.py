de0c1acea527a2a8539077fa4ffbd60e65bcbe0f
Qwlouse <klaus.greff@gmx.net>
2013-03-22 00:35:25 +0100

    made slash after .admin optional

--- findeco/urls.py
+++ findeco/urls.py
@@ -146,10 +146,10 @@ urlpatterns = patterns(
         name='account_reset_confirmation'),                       
                        
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^\.admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^\.admin/doc/?', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^\.admin/', include(admin.site.urls)),
+    url(r'^\.admin/?', include(admin.site.urls)),
 )
 
 handler404 = 'findeco.views.error_404'
