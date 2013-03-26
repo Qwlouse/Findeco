e45c36d34bc5ed4b00786a5cf8e574e6f9b63baf
pinae <pinae@web.de>
2013-03-26 01:35:59 +0100

    it makes more sense to search for texts than for nodes

--- findeco/views.py
+++ findeco/views.py
@@ -281,10 +281,11 @@ def mark_user_unfollow(request, name):
 @ViewErrorHandling
 def search(request, search_fields, search_string):
     node_query = get_query(search_string, ['text', ])
-    found_nodes = backend.Node.objects.filter(node_query).order_by("-id")
+    found_texts = backend.Text.objects.filter(node_query).order_by("-id")
     search_results = []
-    for node in found_nodes:
-        search_results.append({"url": node.getTextPath(), "snippet": node.text[:min(len(node.text), 140)]})
+    for text_node in found_texts:
+        search_results.append({"url": text_node.node.get_a_path(),
+                               "snippet": text_node.text[:min(len(text_node.text), 140)]})
     return json_response({'searchResponse': [{'searchField': "content", 'searchEntries': search_results}]})
 
 
