d62a6b79c5df6c19d5e03007b030a792eeb14e03
Qwlouse <klaus.greff@gmx.net>
2013-04-08 22:53:55 +0200

    hacked the creole lib to not need a whitespace after equals signs in headings. fixes #95

--- static/js/lib/codeholicsCreole.js
+++ static/js/lib/codeholicsCreole.js
@@ -334,7 +334,7 @@ Parse.Simple.Creole = function(options) {
 
     for (var i = 1; i <= 6; i++) {
         g['h' + i] = { tag: 'h' + i, capture: 2,
-            regex: '(^|\\n)[ \\t]*={' + i + '}[ \\t]' +
+            regex: '(^|\\n)[ \\t]*={' + i + '}(?!=)[ \\t]?' +
                 '([^~]*?(~(.|(?=\\n)|$))*)[ \\t]*=*\\s*(\\n|$)'
         };
     }
