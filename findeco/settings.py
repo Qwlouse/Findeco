32f766fcc203e51c7bbf41923283f574b7eb36ab
Justelex <github.acc@maik-nauheim.de>
2013-04-29 00:04:29 +0200

    cleanup of settings.py (bug is known)

--- findeco/settings.py
+++ findeco/settings.py
@@ -28,25 +28,35 @@
 # Django settings for findeco project.
 from __future__ import division, print_function, unicode_literals
 
-import os
-import datetime
 
+
+
+
+###############################################################################
+#                  Copy Values to your Local Settings File 
+#                        Start Here
+#↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+# do not change the following six lines
+import datetime
+import os
 PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                            os.path.pardir))
-
-
 def projectPath(filename):
     return os.path.join(PROJECT_DIR, filename)
 
+#You should turn debug off on public machines. This is helpfull for development and Setup
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
+# We will send Exceptions created by the Api to this Email
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Your Name', 'your_name@email.com'),
 )
 
-MANAGERS = ADMINS
+#The admin password used on first database Sync make shure to change your Password after first login  
+ADMIN_PASS = "1234"
 
+# You can define your Database here. The default configuration (sglite) works fine for testing and development environments
 DATABASES = {
     'default': {
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -61,6 +71,8 @@ DATABASES = {
         'PORT': '',
     }
 }
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'MQ4DNT74V5FFRZNFKC01CI6DYERAXEJZO5DNURVV2G4NTZ5B6OFGFPGJGKRT'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -68,37 +80,58 @@ DATABASES = {
 # In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'Europe/Berlin'
 
+
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'de-DE'
+
+# Settings for sending registration and other mails to users 
+EMAIL_HOST = ''
+EMAIL_PORT = 25
+EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = ''
+EMAIL_SUBJECT_PREFIX = '[Findeco]'
+EMAIL_USE_TLS = True
+
+# This url is used in mails and similar things for pointing to your Findeco System
+FINDECO_BASE_URL = 'http://127.0.0.1:8000'
+
+
+# Drop Activation keys and Recovery keys after the configured time
+ACTIVATION_KEY_VALID_FOR = datetime.timedelta(hours=24)
+RECOVERY_KEY_VALID_FOR = datetime.timedelta(hours=24)
+
+
+# These Strings are send on registration and Mail recovery (They will change there place in the near future
+REGISTRATION_TITLE = 'Your Findeco registration'
+REGISTRATION_BODY = 'You have registred an Findeco Account with this E-Mail ' \
+                    'address. You can activate it with the Link below. If you' \
+                    ' did not register it you just need to ignore this Mail '
+REGISTRATION_RECOVERY_TITLE = 'You requested a Password reset'
+REGISTRATION_RECOVERY_BODY = 'You requested an Password reset on your Findeco' \
+                             ' Account. You can request it with the Link ' \
+                             'below. If you did not request it you just need' \
+                             ' to ignore this Mail '
+REGISTRATION_RECOVERY_TITLE_SUCCESS = 'Your new Findeco Password'
+REGISTRATION_RECOVERY_BODY_SUCCESS = 'Your new Findeco password is'
+
+#↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+#                    End of Values for Local Settings File
+###############################################################################
+
 
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
-USE_L10N = True
+USE_L10N = False
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = False
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -119,14 +152,9 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '+90q803@g1p7s$ks8o0v%t7ojxdg_g+pvf5lz%gf$1b*$^ih!l'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
+
+
 
 CSRF_COOKIE_NAME = str("XSRF-TOKEN")
 
@@ -143,11 +171,7 @@ ROOT_URLCONF = 'findeco.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'findeco.wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    PROJECT_DIR + '/templates/',
-)
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -196,37 +220,7 @@ LOGGING = {
     }
 }
 
-# django-registration
-# Registration settings
-
-ADMINS = (
-    ('Your Name', 'your_name@email.com'),
-)
-
-EMAIL_HOST = ''
-EMAIL_PORT = 25
-EMAIL_HOST_PASSWORD = ''
-EMAIL_HOST_USER = ''
-EMAIL_SUBJECT_PREFIX = '[Findeco]'
-EMAIL_USE_TLS = True
-FINDECO_BASE_URL = 'http://127.0.0.1:8000'
-REGISTRATION_TITLE = 'Your Findeco registration'
-REGISTRATION_BODY = 'You have registred an Findeco Account with this E-Mail ' \
-                    'address. You can activate it with the Link below. If you' \
-                    ' did not register it you just need to ignore this Mail '
-REGISTRATION_RECOVERY_TITLE = 'You requested a Password reset'
-REGISTRATION_RECOVERY_BODY = 'You requested an Password reset on your Findeco' \
-                             ' Account. You can request it with the Link ' \
-                             'below. If you did not request it you just need' \
-                             ' to ignore this Mail '
-REGISTRATION_RECOVERY_TITLE_SUCCESS = 'Your new Findeco Password'
-REGISTRATION_RECOVERY_BODY_SUCCESS = 'Your new Findeco password is'
-
 APPEND_SLASH = False
-
-ACTIVATION_KEY_VALID_FOR = datetime.timedelta(hours=24)
-RECOVERY_KEY_VALID_FOR = datetime.timedelta(hours=24)
-ADMIN_PASS = "1234"
 
 CRON_POLLING_FREQUENCY = 300  # in seconds
 
