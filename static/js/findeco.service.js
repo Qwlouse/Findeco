fd3eece0994ffa100a9db4a9297d706ee0bb8c15
Qwlouse <klaus.greff@gmx.net>
2013-07-11 22:37:50 +0200

    added segment info to Navigator

--- static/js/findeco.service.js
+++ static/js/findeco.service.js
@@ -440,6 +440,7 @@ angular.module('FindecoServices', [])
 
         location.updatePath = function () {
             var path = $location.path();
+            location.segments = $location.search();
             location.parts = path.split("/").filter(isNonEmpty);
             location.path = '/' + location.parts.join("/");
             location.prefix = "";
@@ -481,6 +482,7 @@ angular.module('FindecoServices', [])
                 var arg_parts = nodes[nodes.length - 1].split('.');
                 location.entries.push({name : arg_parts[2] + '.' + arg_parts[3], path : location.argumentPath});
             }
+            console.log(location);
         };
 
         location.getPathForNode = function (shortTitle, index) {
