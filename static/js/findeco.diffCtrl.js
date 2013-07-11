e82d0521f0eeaa784de74bb9fa8e2e5f767b01c3
pinae <pinae@web.de>
2013-07-11 23:26:44 +0200

    diff controller created

---
+++ static/js/findeco.diffCtrl.js
@@ -0,0 +1,33 @@
+/****************************************************************************************
+ * Copyright (c) 2013 Klaus Greff, Maik Nauheim, Johannes Merkert                       *
+ *                                                                                      *
+ * This file is part of Findeco.                                                        *
+ *                                                                                      *
+ * Findeco is free software; you can redistribute it and/or modify it under             *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 3 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * Findeco is distributed in the hope that it will be useful, but WITHOUT ANY           *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * Findeco. If not, see <http://www.gnu.org/licenses/>.                                  *
+ ****************************************************************************************/
+
+/****************************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public                  *
+ * License, v. 2.0. If a copy of the MPL was not distributed with this                  *
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.                             *
+ ****************************************************************************************/
+
+'use strict';
+/* Controllers */
+
+function FindecoDiffCtrl($scope, Backend, User) {
+    $scope.path1 = "/this.4/is.2/an.1/example.12";
+    $scope.path2 = "/this.4/is.2/another.1/example.1";
+}
+
+FindecoDiffCtrl.$inject = ['$scope', 'Backend', 'User'];
