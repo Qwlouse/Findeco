0471cc315f3135867524dfda9af9c15e946a33f4
pinae <pinae@web.de>
2013-10-22 21:39:38 +0200

    refined test

--- microblogging/tests/test_system_messages.py
+++ microblogging/tests/test_system_messages.py
@@ -22,10 +22,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from __future__ import division, print_function, unicode_literals
 from django.test import TestCase
-from node_storage.factory import create_user
+from node_storage.factory import create_user, create_nodes_for_path
 from node_storage.path_helpers import get_root_node
 from findeco.models import get_system_user
-from microblogging.system_messages import post_node_was_flagged_message
+from microblogging.system_messages import post_node_was_flagged_message, post_node_was_unflagged_message
+from microblogging.system_messages import post_new_derivate_for_node_message
 from microblogging.models import Post
 
 
@@ -39,17 +40,35 @@ class ViewHelpersTest(TestCase):
         self.assertIn(hugo, post.mentions.all())
         self.assertEqual(
             post.text_cache,
-            '<span style="color: gray;">Hinweis:</span> '+
+            '<span style="color: gray;">Hinweis:</span> ' +
             '<a href="/user/hugo">Hugo</a> hat <a href="/">/</a> als Spam markiert.')
 
     def test_post_node_was_unflagged_message(self):
         hugo = create_user('Hugo')
-        post = post_node_was_flagged_message('/', hugo)
+        post = post_node_was_unflagged_message('/', hugo)
         self.assertEqual(post.author, get_system_user())
         self.assertEqual(post.location, get_root_node())
         self.assertEqual(post.post_type, Post.SPAM_UNMARKED)
         self.assertIn(hugo, post.mentions.all())
         self.assertEqual(
             post.text_cache,
-            '<span style="color: gray;">Hinweis:</span> '+
+            '<span style="color: gray;">Hinweis:</span> ' +
             '<a href="/user/hugo">Hugo</a> hat die Spam-Markierung für <a href="/">/</a> entfernt.')
+
+    def test_post_new_derivate_for_node_message(self):
+        hugo = create_user('Hugo')
+        node_A = create_nodes_for_path('/bla.1/blubb.1', hugo)
+        node_B = create_nodes_for_path('/bla.2/pling.1', hugo)
+        post = post_new_derivate_for_node_message(hugo, '/bla.1/blubb.1', '/bla.2/pling.1')
+        self.assertEqual(post.author, get_system_user())
+        self.assertEqual(post.location, node_A)
+        self.assertEqual(post.post_type, Post.NODE_REFINED)
+        self.assertIn(hugo, post.mentions.all())
+        self.assertIn(node_A, post.node_references.all())
+        self.assertIn(node_B, post.node_references.all())
+        self.assertEqual(post.node_references.count(), 2)
+        self.assertEqual(
+            post.text_cache,
+            '<span style="color: gray;">Hinweis:</span> ' +
+            '<a href="/user/hugo">Hugo</a> hat <a href="/bla.1/blubb.1">blubb_long</a> zu ' +
+            '<a href="/bla.2/pling.1">pling_long</a> weiterentwickelt.')
