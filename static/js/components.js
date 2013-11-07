42690fcab6f6ace8a79a88963d0da1c77f00416d
justelex <github.acc@maik-nauheim.de>
2013-11-07 17:30:23 +0100

    added directive for ngBindHtmlUnsafe after removal in angular 1.2

--- static/js/components.js
+++ static/js/components.js
@@ -185,7 +185,15 @@ findecoApp
 
         };
     }
-    );
+    )
+    .directive('ngBindHtmlUnsafe', [function() {
+    return function(scope, element, attr) {
+        element.addClass('ng-binding').data('$binding', attr.ngBindHtmlUnsafe);
+        scope.$watch(attr.ngBindHtmlUnsafe, function ngBindHtmlUnsafeWatchAction(value) {
+            element.html(value || '');
+        });
+    }
+}]);
 
 
 
