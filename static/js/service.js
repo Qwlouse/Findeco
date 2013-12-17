4b75deb6ffabd3be4f4cee7881d935989e81d3da
justelex <github.acc@maik-nauheim.de>
2013-12-17 22:05:52 +0100

    Fixed _unkown node errors on unknown nodes Fixes #248

--- static/js/service.js
+++ static/js/service.js
@@ -65,7 +65,7 @@ angular.module('FindecoServices', [])
         $locationProvider.html5Mode(true);
         $locationProvider.hashPrefix('!');
     })
-    .factory('errorHandler', function ($q, Message) {
+    .factory('errorHandler', function ($q, Message,Navigator) {
         return function (promise) {
             return promise.then(
                 function (response) {
@@ -73,8 +73,14 @@ angular.module('FindecoServices', [])
                 },
                 function (response) {
                     if (response.data.errorResponse != undefined) {
-                        Message.send("error", response.data.errorResponse.errorID);
-                        delete response.data.errorResponse;
+                        if (response.data.errorResponse.errorID== "_UnknownNode") {
+                            Message.send('error', 'Die angeforderte Seite existiert nicht');
+                            Navigator.changePath("/");
+                        }else{
+                            Message.send("error", response.data.errorResponse.errorID);
+                            delete response.data.errorResponse;
+                        }
+
                     }
                     return $q.reject(response);
                 }
@@ -221,12 +227,14 @@ angular.module('FindecoServices', [])
 
                 var url = ['/.json_loadNode', path].join('/');
                 var promise = $http.get(url);
-                promise.success(function (d) {
+                promise.success(function (d){
+
                     if((d.loadNodeResponse.nodeID=="1")&&(Navigator.path!="/index")){
                         Message.send('error', 'Die angeforderte Seite existiert nicht');
                         Navigator.changePath("/");
-                               
+
                     }
+
                     angular.copy(d.loadNodeResponse, nodeInfo);
                 });
                 return promise;
