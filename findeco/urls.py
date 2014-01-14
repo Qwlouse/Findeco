c927a06b676e36fbfef2b93db1ac83b67ad3ef89
Qwlouse <klaus.greff@gmx.net>
2014-01-14 22:35:26 +0100

    added missing imports to jasmine.html

--- findeco/urls.py
+++ findeco/urls.py
@@ -25,7 +25,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #endregion #####################################################################
-from djangojs.views import JasmineView
 from findeco import settings
 from libs import django_cron
 from django.conf.urls import patterns, include, url
