534be1e76bddb7fd844400f7743a7092484e1d86
Qwlouse <klaus.greff@gmx.net>
2014-01-14 23:19:43 +0100

    added more elaborate tests for userService

--- static/tests/userService.specs.js
+++ static/tests/userService.specs.js
@@ -1,7 +1,7 @@
 
 
 describe('FindecoUserService', function () {
-    var userService;
+    var userService, httpBackend;
 
     //excuted before each "it" is run.
     beforeEach(function (){
@@ -10,13 +10,63 @@ describe('FindecoUserService', function () {
       angular.mock.module('FindecoUserService');
 
       //inject your service for testing.
-      angular.mock.inject(function(User) {
-        userService = User;
+      angular.mock.inject(function($httpBackend, User) {
+          userService = User;
+          httpBackend = $httpBackend
       });
     });
 
-    //check to see if it has the expected function
-    it('should have an exciteText function', function () {
+    it('should initialize with a .json_loadUserSettings call', function () {
+        httpBackend.expectGET('/.json_loadUserSettings/').respond(406, '');
+        httpBackend.flush();
+        expect(userService.isLoggedIn).toBe(false);
+    });
+
+    it('should have a login function', function () {
         expect(angular.isFunction(userService.login)).toBe(true);
+    });
+
+    it('should set the userInfo details after succesful login', function () {
+        //set up some data for the http call to return and test later.
+        var returnData = {
+            loginResponse: {
+                userInfo: {
+                    displayName: 'hugo',
+                    description: 'beschreibung'
+                },
+                userSettings: {
+                    rsskey: 'abcdefg',
+                    email: 'hugo@abc.de',
+                    followees: []
+                }
+            }
+        };
+        // flush the initial loadUserSettings
+        httpBackend.expectGET('/.json_loadUserSettings/').respond(406, '');
+        httpBackend.flush();
+
+        httpBackend.expectPOST('/.json_login/').respond(returnData);
+
+        //create an object with a function to spy on.
+        var test = {
+          handler: function() {}
+        };
+        spyOn(test, 'handler');
+
+        //make the call.
+        userService.login('hugo', '1234').then(test.handler);
+        httpBackend.flush();
+
+        expect(test.handler).toHaveBeenCalled();
+        expect(userService.isLoggedIn).toBe(true);
+        expect(userService.displayName).toBe('hugo');
+        expect(userService.description).toBe('beschreibung');
+        expect(userService.rsskey).toBe('abcdefg');
+        expect(userService.email).toBe('hugo@abc.de');
+        expect(userService.followees).toBe([]);
+    });
+
+    it('should have a logout function', function () {
+        expect(angular.isFunction(userService.logout)).toBe(true);
     });
 });
