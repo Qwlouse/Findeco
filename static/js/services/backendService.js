618846302cdcd0dbb93ceaa4a97d6c50a7c6c740
jonny <jonny@pinae.net>
2014-12-07 00:02:58 +0900

    Extended backend for storing arguments

--- static/js/services/backendService.js
+++ static/js/services/backendService.js
@@ -159,6 +159,13 @@ angular.module('FindecoBackendService', [])
                 return $http.post(url, data);
             },
 
+            storeArgument: function (path, data) {
+                var pathComponents = ['/.storeArgument', path];
+                var url = pathComponents.join('/');
+                url = url.replace("//", "/");
+                return $http.post(url, data);
+            },
+
             storeText: function (path, params) {
                 var pathComponents = ['/.storeText', path];
                 var url = pathComponents.join('/');
