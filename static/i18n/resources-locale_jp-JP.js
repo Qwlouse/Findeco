0cbc7dff84026aaf2e52d4a1725f7f290dcf1ec9
jonny <jonny@pinae.net>
2014-12-22 01:31:16 +0900

    Corrected argument type description

--- static/i18n/resources-locale_jp-JP.js
+++ static/i18n/resources-locale_jp-JP.js
@@ -1310,7 +1310,7 @@
     },
     {
         "key"        : "_ArgumentTypeHelp_",
-        "value"      : "Next to the proposal is a separate list of arguments for each proposal type in the arguments sidebar. Other users can click the star text to your argument which influeces the sorting of the arguments in the list with the most liked arguments at the top. Make sure to choose the right category for your argument because it will not get this support if it is in the wrong category.",
+        "value"      : "The argument type will be displayed left of the argument heading. Other users can click the star right of the heading of your argument which influeces the sorting of the arguments in the list. The most liked arguments are sorted to the top. Make sure to choose the right category for your argument because it will not get this support if it is in the wrong category.",
         "description": ""
     }
 ]
